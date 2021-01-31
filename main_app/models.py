45761af4275037274ccea5d261117a319748d0ef
sebastians9517 <sebastians9517@gmail.com>
2021-01-31 15:36:34 -0600

    finished and working

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.urls import reverse
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 # Create your models here.
 class Dog(models.Model):
@@ -6,3 +13,30 @@ class Dog(models.Model):
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
+
+  def __str__(self):
+      return self.name
+
+    # This is another way of redirecting after creating a dog
+  def get_absolute_url(self):
+      return reverse('detail', kwargs={'dog_id': self.id})
+
+  class Meta:
+      ordering = ['id']
+
+class Feeding(models.Model):
+    date = models.DateField('Feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+
+    # This creates a Foreign Key (FK) and deletes all the information related to a deleted cat so there is no data related to something that doesn't exist anymore (no orphan data)
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
